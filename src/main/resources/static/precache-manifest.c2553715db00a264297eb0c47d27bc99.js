d9bc08c5647b4dd8f54f5f88db1ec538b5107800
Stephen <xxw11165@gmail.com>
2020-11-08 18:20:48 -0800

    add react end

---
+++ src/main/resources/static/precache-manifest.c2553715db00a264297eb0c47d27bc99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b5d8f55de58d8473e1832e57857dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac8d2c514453f35c27c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "194bd3ea31a2e5f26c7a",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "1ac8d2c514453f35c27c",
+    "url": "/static/js/2.b4af352e.chunk.js"
+  },
+  {
+    "revision": "6177d1ab9ae3c0c435b4d0c754a1b33b",
+    "url": "/static/js/2.b4af352e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194bd3ea31a2e5f26c7a",
+    "url": "/static/js/main.87986828.chunk.js"
+  },
+  {
+    "revision": "6437c456997fa8856a3f",
+    "url": "/static/js/runtime-main.010595a5.js"
+  }
+]);
